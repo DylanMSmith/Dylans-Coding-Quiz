a21bc4c21484e9731cd1f954c185b9a39221fad0
Dylan Smith <dsreload@gmail.com>
2023-02-20 20:27:20 -0500

    js update

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+var body = document.querySelector("body");
+var timer = document.querySelector("#time");
+var instructions = document.querySelector(".instructions");
+var beginButtonClick = document.querySelector("#beginButton");
+var questionP = document.querySelector(".question");
+var choicesBlock = document.querySelector(".choices");
+var result = document.querySelector(".result");
+var finalScore = document.querySelector(".final-score");
+var endOfGame = document.querySelector(".end-of-game");
+
+var buttonAnswerA = document.querySelector("#answer1");
+var buttonAnswerB = document.querySelector("#answer2");
+var buttonAnswerC = document.querySelector("#answer3");
+var buttonAnswerD = document.querySelector("#answer4");
+
+var submitInitials = document.querySelector(".initial-form");
+
+var currentTime = timer.textContent;
+var currentQuestion = 0;
